b8fd18c4eeb3ead2513fc5f1fa49e4f1b8bd85f5
Otoniel Guajardo <otonielguajardo@yahoo.com>
2021-06-02 18:33:45 -0500

    code cleanup

--- index.ts
+++ index.ts
@@ -7,16 +7,18 @@ const start = async () => {
     //
     const x_current_date = '15-feb-21';
     const x_interest = 7.5;
+    const x_tax = 16;
     const x_commercial_year_days = 360;
     let affected_accounts: Array<number> = [];
 
     console.log('\x1b[33m%s\x1b[0m', '# Inicialización de Base de Datos completada');
 
+    console.log('\x1b[33m%s\x1b[0m', '# Todas las cuentas');
+    let all_accounts: Array<AccountModel> = await database('accounts').select('*');
+    console.table(all_accounts);
+
     // recorrer la tabla de Cuentas de Débito que están Activas
     let accounts: Array<AccountModel> = await database('accounts').select('*').where('status', 'active');
-
-    console.log('\x1b[33m%s\x1b[0m', '# Cuentas Activas');
-    console.table(accounts);
 
     await asyncForEach(accounts, async (account: AccountModel) => {
         //
@@ -31,8 +33,13 @@ const start = async () => {
         // realizar el cobro de Pagos con Estado Pendiente de la tabla de Préstamos
         await asyncForEach(loans, async (loan: LoanModel) => {
             //
+            let term = moment(x_current_date, BAN_DATE_FORMAT).diff(moment(loan.date, SQL_DATE_FORMAT), 'days');
+            let interest = loan.amount * term * (x_interest / x_commercial_year_days);
+            let tax = interest * x_tax;
+            let loan_amount = loan.amount + interest + tax;
+
             // Considera que se realiza el cobro solo de los Pagos que se puedan realizar completos, es decir que el saldo en la Cuenta sea mayor al Pago.
-            if (account_balance > loan.amount) {
+            if (account_balance > loan_amount) {
                 //
                 // Si se aplica el Pago es necesario cambiar el Estado del Préstamo de Pendiente a Pagado
                 await database('loans')
@@ -40,30 +47,22 @@ const start = async () => {
                     .where('id', loan.id)
                     .then(async () => {
                         //
-                        let term = moment(x_current_date, BAN_DATE_FORMAT).diff(
-                            moment(loan.date, SQL_DATE_FORMAT),
-                            'days'
-                        );
-                        let interest = (loan.amount * term * x_interest) / x_commercial_year_days;
-                        let tax = interest * x_interest;
-                        let amount = loan.amount + interest + tax;
-
                         console.log(
                             `# ${account.id} pagó el préstamo ${loan.id} (${loan.amount.toFixed(
                                 2
-                            )}) | ${account_balance.toFixed(2)} - ${amount.toFixed(2)} = ${(
-                                account_balance - amount
+                            )}) | ${account_balance.toFixed(2)} - ${loan_amount.toFixed(2)} = ${(
+                                account_balance - loan_amount
                             ).toFixed(2)}`
                         );
 
-                        account_balance = account_balance - amount;
+                        account_balance = account_balance - loan_amount;
                         affected_accounts.push(account.id);
 
                         let payload = {
                             account_id: account.id,
                             client_id: account.client_id,
                             term,
-                            amount: amount.toFixed(2),
+                            amount: loan_amount.toFixed(2),
                             interest: interest.toFixed(2),
                             tax: tax.toFixed(2),
                         };
